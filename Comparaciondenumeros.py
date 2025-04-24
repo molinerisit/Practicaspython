5a7c0b74228805edf6c72190d381dcaaaba0882d
molinerisit <julianmolinerisit@gmail.com>
2025-04-24 19:18:59 -0300

    Ejercicio de comparacion de números

---
+++ Comparaciondenumeros.py
@@ -0,0 +1,19 @@
+n1 = 6
+n2 = 4
+
+if n1 == n2: 
+    print ("son iguales")
+else: 
+    print ("Son distintos")
+if n1 != n2:
+    print ("Son distintos")
+else: 
+    print ("Son iguales")
+if n1 > n2:
+    print (n1 , "Es mayor que", n2)
+else:
+    print(n2, "Es mayor que", n1)
+if n1 >= n2:
+    print (n1 , "Es mayor o igual que", n2)
+else: 
+    print(n1,"No es mayor o igual que:",n2)
