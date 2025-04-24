b8e72a06738404b15f46f6334125ad30e580b418
molinerisit <julianmolinerisit@gmail.com>
2025-04-24 20:10:35 -0300

    Aleatorios

---
+++ Aleatorios.py
@@ -0,0 +1,26 @@
+import random
+x = random.random()
+print(x)
+
+# Numero aleatorio entero igual o mayor que 2 y menor a 10.
+
+import random
+ejemplo = random.randrange(2,10)
+print(ejemplo)
+
+
+# Numero aleatorio mayor o igual que 20 y menor o igual que 100.
+
+import random
+ejemplo = random.randint(2,10)
+print(ejemplo)
+
+
+import random
+secuencia = 2,3,4,5,6,7,8
+secuencia1 = "hola","chau","gracias"
+
+ejemplo1 = random.choice(secuencia)
+ejemplo2 = random.choice(secuencia1)
+print(ejemplo1, "", ejemplo2)
+
