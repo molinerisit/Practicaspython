85204fe3cee1294fcf64afc9eb0399a41cd5dd1b
molinerisit <julianmolinerisit@gmail.com>
2025-04-24 19:26:48 -0300

    Comparación de cadenas

--- Comparaciondenumeros.py
+++ Comparaciondenumeros.py
@@ -1,3 +1,6 @@
+# Comparación de números
+
+
 n1 = 6
 n2 = 4
 
@@ -17,3 +20,25 @@ if n1 >= n2:
     print (n1 , "Es mayor o igual que", n2)
 else: 
     print(n1,"No es mayor o igual que:",n2)
+
+# Comparación de cadenas
+
+cn1 = "Hola"
+cn2 = "Chau"
+
+if cn1 == cn2: 
+    print ("son iguales")
+else: 
+    print ("Son distintos")
+if cn1 != "Hola":
+    print ("No es Hola")
+else: 
+    print ("Es Hola")
+
+cad1 = "mesa"
+cad2 = "Tambor"
+
+if cad1 > cad2:
+    print ("Orden de longitud:", cad1, cad2)
+else:
+    print("Orden de longitud:", cad2, cad1)
