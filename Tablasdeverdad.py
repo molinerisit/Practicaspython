211ae7b4aa755d06d902d60c8e3031293e21b79f
molinerisit <julianmolinerisit@gmail.com>
2025-04-24 19:50:48 -0300

    Tablas de verdad

---
+++ Tablasdeverdad.py
@@ -0,0 +1,31 @@
+# Operador AND
+
+sueldo = 160000
+antiguedad = 10
+
+if sueldo > 150000 and antiguedad >= 10:
+    print ("Crédito concedido")
+else:
+    print ("Rechazado")
+
+# Operador or
+
+angulo1 = 90
+angulo2 = 100
+angulo3 = 120
+
+if angulo1 == 90 or angulo2 == 90 or angulo3 == 90:
+    print("Es un triangulo rectangulo.")
+else:
+    print("No es un triangulo rectangulo.")
+
+# Operador not 
+
+edad = 17
+
+if not edad >= 18:
+    print("No puede votar")
+else:
+    print("Puede votar.")
+
+# la negacion seria: no 18, es decir que la edad es menor a 18.
